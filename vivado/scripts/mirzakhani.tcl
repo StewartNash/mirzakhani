743f65eae95fd423f1d7935f7a9ac4673e67591d
StewartNash <Stewart.M.Nash@gmail.com>
2026-03-30 18:59:50 -0700

    Updated TCL

--- vivado/scripts/mirzakhani.tcl
+++ vivado/scripts/mirzakhani.tcl
@@ -140,7 +140,7 @@ set proj_dir [get_property directory [current_project]]
 # Set project properties
 set obj [current_project]
 #set_property -name "board_part_repo_paths" -value "[file normalize "$origin_dir/../../../../.Xilinx/Vivado/2025.2/xhub/board_store/xilinx_board_store"]" -objects $obj
-set_property board_part_repo_paths $::env(XILINX_BOARD_STORE) [current_project]
+#set_property board_part_repo_paths $::env(XILINX_BOARD_STORE) [current_project]
 set_property -name "board_part" -value "digilentinc.com:zybo-z7-10:part0:1.2" -objects $obj
 set_property -name "default_lib" -value "xil_defaultlib" -objects $obj
 set_property -name "enable_resource_estimation" -value "0" -objects $obj
