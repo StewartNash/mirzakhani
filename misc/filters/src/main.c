c407e7f7247034ede430148fd8f65c37f57c6706
StewartNash <stewart.m.nash@gmail.com>
2026-04-19 11:24:50 -0700

    Write data to file

--- misc/filters/src/main.c
+++ misc/filters/src/main.c
@@ -4,6 +4,14 @@
 #include "filter.h"
 
 int main(int argc, char *argv[]) {
+	FILE* file;
+	
+	file = fopen("data.txt", "w");
+	if (file == NULL) {
+		printf("ERROR: Could not create data.txt\r\n");
+		return 1;
+	}
+
 	printf("#START\r\n");
 	printf("#RATE=%d\r\n", SAMPLE_RATE_HZ);
 	printf("#PERIOD_US=%d\r\n", SAMPLE_PERIOD_US);
@@ -11,5 +19,15 @@ int main(int argc, char *argv[]) {
 	printf("#UNIT=%s\r\n", UNIT_NAME);
 	printf("#COMMENT=%s\r\n", COMMENT_STR);
 
+	fprintf(file, "#START\r\n");
+	fprintf(file, "#RATE=%d\r\n", SAMPLE_RATE_HZ);
+	fprintf(file, "#PERIOD_US=%d\r\n", SAMPLE_PERIOD_US);
+	fprintf(file, "#CHANNEL=%s\r\n", CHANNEL_NAME);
+	fprintf(file, "#UNIT=%s\r\n", UNIT_NAME);
+	fprintf(file, "#COMMENT=%s\r\n", COMMENT_STR);
+	
+	fclose(file);
 	printf("Done!\r\n");
+
+	return 0;
 }
