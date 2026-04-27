0d50865c8f40031b1d171bd106626a4f6ebaf502
StewartNash <stewart.m.nash@gmail.com>
2026-04-27 00:14:26 -0700

    Added ADC C file

--- misc/filters/include/adc.h
+++ misc/filters/include/adc.h
@@ -4,27 +4,29 @@
 #pragma message("Please undefine DEBUG_TEST.")
 #endif
 
+#include <stdio.h>
+
 #ifndef ADC_H
 #define ADC_H
 
-#include <stdint.h>
-#include <stdlib.h>
-
 #define UUID_SIZE 16
 
-void uuid(char *output);
+struct session {
+	//int sample_rate_hz;
+	float sample_rate_hz;
+	float sample_period_us;
+	char *channel_name;
+	char *unit_name;
+	char *comment_str;
+	
+	int total_samples;
+	int batch_size;
 
-// v4 UUID generator
-void uuid(char *output) {
-  uint8_t bytes[UUID_SIZE];
-  
-  for (int i = 0; i < UUID_SIZE; i++) {
-      bytes[i] = rand() & 0xFF;
-  }
-  // Set version (4) - bits 12-15 of time_hi_and_version
-  bytes[6] = (bytes[6] & 0x0F) | 0x40;
-  // Set variant (FC4122) - bits 6-7 of clock_seq_hi_and_reserved
-  bytes[8] = (bytes[8] & 0x3F) | 0x80;
+	int sysmon_device_id;
 }
+
+void uuid(char *output);
+void print_uuid(char *bytes);
+void fprint_uuid(FILE* file, char *bytes);
 
 #endif
---
+++ misc/filters/src/adc.c
@@ -0,0 +1,50 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "adc.h"
+
+// v4 UUID generator
+void uuid(char *output) {
+	uint8_t bytes[UUID_SIZE];
+
+	for (int i = 0; i < UUID_SIZE; i++) {
+		bytes[i] = rand() & 0xFF;
+	}
+	// Set version (4) - bits 12-15 of time_hi_and_version
+	bytes[6] = (bytes[6] & 0x0F) | 0x40;
+	// Set variant (FC4122) - bits 6-7 of clock_seq_hi_and_reserved
+	bytes[8] = (bytes[8] & 0x3F) | 0x80;
+}
+
+void print_uuid(char *bytes) {
+	sprintf(message,
+		"%02x%02x%02x%02x-"
+		"%02x%02x-"
+		"%02x%02x-"
+		"%02x%02x-"
+		"%02x%02x%02x%02x%02x%02x",
+		bytes[0], bytes[1], bytes[2], bytes[3],
+		bytes[4], bytes[5],
+		bytes[6], bytes[7],
+		bytes[8], bytes[9],
+		bytes[10], bytes[11], bytes[12],
+		bytes[13], bytes[14], bytes[15]
+	);
+}
+
+void fprint_uuid(FILE *file, char *bytes) {
+	fprintf(file,
+		"%02x%02x%02x%02x-"
+		"%02x%02x-"
+		"%02x%02x-"
+		"%02x%02x-"
+		"%02x%02x%02x%02x%02x%02x",
+		bytes[0], bytes[1], bytes[2], bytes[3],
+		bytes[4], bytes[5],
+		bytes[6], bytes[7],
+		bytes[8], bytes[9],
+		bytes[10], bytes[11], bytes[12],
+		bytes[13], bytes[14], bytes[15]
+	);
+}
+
