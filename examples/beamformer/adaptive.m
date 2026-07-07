25c41a2867d5a6ad9e7a135c6fe71396ea3be592
StewartNash <11215965+StewartNash@users.noreply.github.com>
2026-07-06 21:49:18 -0700

    Added some of JCGL code

--- examples/beamformer/adaptive.m
+++ examples/beamformer/adaptive.m
@@ -59,4 +59,77 @@ end
 mse = mean(abs(e_dp(1 + 1, :)).^2);
 fprintf('MSE = %f\n', mse);
 
+%% JCGL (Joint Complex Gradient Lattice)
+
+clear
+
+% Constants (function input parameters)
+
+SAMPLE_FREQUENCY = 1024;
+SAMPLE_LENGTH = 1024;
+FILTER_ORDER = 16;
+ALPHA = 0.01;
+
+% Input signal
+
+fs = SAMPLE_FREQUENCY;
+t = (0 : SAMPLE_LENGTH - 1)' / fs; % Time
+s = exp(1j * 2 * pi * 50 * t); % Desired signal
+noise = 0.5 * (randn(size(t)) * 1j * randn(size(t)));
+x = s + noise; % Input signal
+d = s; % Desired signal
+
+p = FILTER_ORDER;
+N = length(x);
+
+% Filter variables
+
+alpha_JCGL = ALPHA;
+
+E_di = 0; % Minimum average squared error
+E_ei = 0; % Minimum forward prediction error
+E_ri = 0; % Minimum backward prediction error
+delta_ei = 0; % Forward (prediction) discrepancy
+delta_ri = 0; % Backward (prediction) discrepancy
+K_ei = 0; % Forward weighting factor (reflection coefficient / partial correlation coefficient)
+K_ri = 0; % Backward weighting factor (reflection coefficient / partial correlation coefficient)
+K_di = 0; % Weighting factor
+
+e_i = []; % i-th order forward prediction error sequence
+r_i = []; % i-th ortder backward prediction error sequence
+e_di = []; % Error
+
+a_ik = []; % Forward prediction (filter) coefficients
+b_ik = []; % Backward prediction (filter) coefficients
+
+T = conv(x(1 : p), conj(flipud(x(1 : p)))); % Autocorrelation of signal
+E_0 = T;
+epsilon_JCGL = E_0(0 + 1);
+
+%K_i = zeros(p, N + 2)';
+K_i = zeros(p + 1, 2)'; % n - 1, n
+%E_ri = epsilon_JCGL * ones(p, N + 2)';
+E_ri = epsilon_JCGL * ones(p + 1, 3)'; % n - 2, n - 1, n
+%E_ei = epsilon_JCGL * ones(p, N + 2)';
+E_ei = epsilon_JCGL * ones(p + 1, 3)'; % n - 2, n - 1, n
+e_i = zeros(p + 1, 1)';
+r_i = zeros(p + 1, 2)';
+K_di = zeros(p + 1, 1)';
+e_di = zeros(p + 1, 1)';
+a_ik = zeros(p + 1);
+b_ik = zeros(p + 1);
+
+for n = 1 + 2 : 1 : N + 1 + 2
+	K_i(2, :) = K_i(2 - 1, :) - 2 * alpha_JCGL ./ (E_ri(3 - 2, :) + E_ei(2 - 1, :));
+	%K_i(n, :) = K_i(n - 1, :) - 2 * alpha_JCGL ./ (E_ri(n - 2, :) + E_ei(n - 1, :));
+	for i = 1 + 1 : 1 : p + 1
+		e_i(i) = e_i(i - 1) + K_i(2, i) * r_i(2 - 1, i - 1);
+		r_i(2, i) = r_i(2 - 1, i - 1) + conj(K_i(2, i - 1)) * e_i(i - 1);
+		E_ri(3 - 1, i - 1) = (1 - alpha_JCGL) * E_ri(3 - 2, i - 1) + alpha_JCGL * abs(r_i(2 - 1, i - 1))^2;
+		K_di(2, i) = K_di(2 - 1, i) - 2 * alpha_JCGL / (E_ri(2 - 1, i)) * e_di(2 - 1, i) * conj(r_i(2 - 1, i));
+		e_di(i) = e_di(i - 1) + K_di(i) *r_i(2, i);
+	
+
+
+
 
