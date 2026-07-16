9a93e8fd0ba7e9014665b685d4e84f40d70ad608
StewartNash <11215965+StewartNash@users.noreply.github.com>
2026-07-15 21:16:42 -0700

    Finished putting in intial JCGL code

--- examples/beamformer/adaptive.m
+++ examples/beamformer/adaptive.m
@@ -1,3 +1,10 @@
+% Complex Adaptive Joint Process Algorithms
+
+% Example notation
+% h_k(n, i)
+% h_k = zeros(p + 2, N)' --> zeros(i-count + i-offset, n-count)'
+% h_k(-1 + 3, 0 + 2) --> h_k(n-index + n-offset, i-index + i-offset)
+
 %% JCLMS (Joint Complex Gradient Transversal Filter)
 
 clear
@@ -34,6 +41,8 @@ h_k = []; % Filter coefficients
 %a_ik = [] % Forward prediction (filter) coefficients
 %b_ik = [] % Backward prediction (filter) coefficients
 
+% INITIALIZATION
+
 h_k = zeros(p, 2)';
 T = conv(x(1 : p), conj(flipud(x(1 : p)))); % Autocorrelation of signal
 epsilon_JCLMS = T(0 + 1);
@@ -41,6 +50,8 @@ E_e0 = epsilon_JCLMS * ones(1, 2)';
 e_dp = zeros(1, 2)';
 err = [];
 y = [];
+
+% TIME UPDATE
 
 for n = 1 : 1 : N
 	if n > p
@@ -102,6 +113,8 @@ N = length(x);
 
 alpha_JCGL = ALPHA;
 
+% INITIALIZATION
+
 E_di = 0; % Minimum average squared error
 E_ei = 0; % Minimum forward prediction error
 E_ri = 0; % Minimum backward prediction error
@@ -119,48 +132,70 @@ a_ik = []; % Forward prediction (filter) coefficients
 b_ik = []; % Backward prediction (filter) coefficients
 
 T = conv(x(1 : p), conj(flipud(x(1 : p)))); % Autocorrelation of signal
-E_0 = T;
-epsilon_JCGL = E_0(0 + 1);
+E_e0 = T;
+epsilon_JCGL = E_e0(0 + 1);
 
 %K_i = zeros(p, N + 2)';
 K_i = zeros(p + 1, 2)'; % n - 1, n
 %E_ri = epsilon_JCGL * ones(p, N + 2)';
-E_ri = epsilon_JCGL * ones(p + 1, 3)'; % n - 2, n - 1, n
+E_ri = epsilon_JCGL * ones(p + 2, 3)'; % n - 2, n - 1, n
 %E_ei = epsilon_JCGL * ones(p, N + 2)';
-E_ei = epsilon_JCGL * ones(p + 1, 3)'; % n - 2, n - 1, n
-e_i = zeros(p + 1, 1)';
-r_i = zeros(p + 1, 2)';
-K_di = zeros(p + 1, 1)';
-e_di = zeros(p + 1, 1)';
-a_ik = zeros(p + 1);
-b_ik = zeros(p + 1);
+E_ei = epsilon_JCGL * ones(p + 2, 2)'; % n - 1, n
+e_i = zeros(p + 2, 2)';
+r_i = zeros(p + 2, 3)';
+K_di = zeros(p + 1, 2)';
+e_di = zeros(p + 2, 2)';
+a_ik = zeros(p + 2);
+b_ik = zeros(p + 2);
 
-for n = 1 + 2 : 1 : N + 1 + 2
-	K_i(2, :) = K_i(2 - 1, :) - 2 * alpha_JCGL ./ (E_ri(3 - 2, :) + E_ei(2 - 1, :));
-	%K_i(n, :) = K_i(n - 1, :) - 2 * alpha_JCGL ./ (E_ri(n - 2, :) + E_ei(n - 1, :));
-	for i = 1 + 1 : 1 : p + 1
-		e_i(i) = e_i(i - 1) + K_i(2, i) * r_i(2 - 1, i - 1);
-		r_i(2, i) = r_i(2 - 1, i - 1) + conj(K_i(2, i - 1)) * e_i(i - 1);
-		E_ri(3 - 1, i - 1) = (1 - alpha_JCGL) * E_ri(3 - 2, i - 1) + alpha_JCGL * abs(r_i(2 - 1, i - 1))^2;
-		K_di(2, i) = K_di(2 - 1, i) - 2 * alpha_JCGL / (E_ri(2 - 1, i)) * e_di(2 - 1, i) * conj(r_i(2 - 1, i));
-		e_di(i) = e_di(i - 1) + K_di(i) * r_i(2, i);
-		for k = 1 : 1 : p
-			a_ik(i, i) = K_i(2, i);
-			if k < 1
-				a_ik(i, k) = a_ik(i - 1, k) + K_i(2, i) * a_ik(i - k, i - 1);
+err = [];
+y = [];
+
+for n = 1 : 1 : N
+	% TIME UPDATE
+	e_i(0 + 2, 0 + 2) = x(n);
+	r_i(0 + 3, 0 + 2) = x(n);
+	e_di(0 + 2, -1 + 2) = d(n);
+	
+	% ORDER UPDATE
+	for i = 0 : 1 : p
+		temporary_factor = e_i(-1 + 2, i + 2) .* conj(r_i(-2 + 3, i - 1 + 2));
+		temporary_factor = temporary_factor + conj(r_i(-1 + 3, i + 2)) .* e_i(-1 + 2, i - 1 + 2);
+		temporary_factor = abs(temporary_factor);
+		K_i(0 + 2, i + 1) = K_i(-1 + 2, i + 1) - 2 * alpha_JCGL / (E_ri(-2 + 3, i - 1 + 2) + E_ei(-1 + 2, i - 1 + 2)) * temporary_factor;
+		if i ~= 0
+			e_i(0 + 2, i + 2) = e_i(0 + 2, i - 1 + 2) + K_i(0 + 2, i + 1) * r_i(-1 + 3, i - 1 + 2);
+			r_i(0 + 3, i + 2) = r_i(-1 + 3, i - 1 + 2) + conj(K_i(0 + 2, i + 1)) * e_i(0 + 2, i - 1 + 2);
+			E_ri(-1 + 3, i - 1 + 2) = (1 - alpha_JCGL) * E_ri(-2 + 3, i - 1 + 2) + alpha_JCGL * abs(r_i(-1 + 3, i - 1 + 2))^2;
+			E_ei(0 + 2, i - 1 + 2) = (1 - alpha_JCGL) * E_ei(-1 + 2, i - 1 + 2) + alpha_JCGL * abs(e_i(0 + 2, i - 1 + 2))^2;
+		end
+		K_di(0 + 2, i + 1) = K_di(-1 + 2, i + 1) - 2 * alpha_JCGL / (E_ri(-1 + 3, i + 2)) * e_di(-1 + 2, i + 2) * conj(r_i(-1 + 3, i - 1 + 2));
+		e_di(0 + 2, i + 2) = e_di(0 + 2, i - 1 + 2) + K_di(0 + 2, i + 1) * r_i(0 + 2, i + 1);
+		
+		% PREDICTORS
+		for k = 0 : 1 : p
+			a_ik(i + 2, i + 2) = K_i(0 + 2, i + 1); % Move this outside k-loop
+			if k >= 1 && k <= i - 1
+				a_ik(i + 2, k + 2) = a_ik(i - 1 + 2, k + 2) + K_i(0 + 2, i + 1) * conj(a_ik(i - k + 2, i - 1 + 2));
 			end
 		end
 		for k = 0 : 1 : p
-			b_ik(k + 1) = conj(a_ik(i, p - k + 1));
+			b_ik(i + 2, k + 2) = conj(a_ik(i + 2, p - k + 2));
 		end
 	end
+	err = [err; e_di(0 + 2, p + 2)];
+	diagonal_aik = diag(a_ik);
+	y = [y; -diagonal_aik(0 + 2, p + 2)' * r_i(0 + 3, 0 + 2 : p + 2)'];
 end
 
 % Output
 
-mse = mean(abs(e_di).^2);
-
+mse = mean(abs(err).^2);
 fprintf('MSE = %f\n', mse);
+figure_2 = figure;
+plot(1 : N, abs(d), 1 : N, abs(y), 1 : N, abs(x));
+legend('Desired Signal', 'Estimate', 'Input Signal');
+title('JCGL');
 
 %% JCLSL (Joint Complex Least Squares Lattice)
 
