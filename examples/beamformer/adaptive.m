ffcfd0b5f5fd73beab9effc1d1c651de9ac534f6
StewartNash <11215965+StewartNash@users.noreply.github.com>
2026-07-21 20:35:55 -0700

    Finished JCGL

--- examples/beamformer/adaptive.m
+++ examples/beamformer/adaptive.m
@@ -83,7 +83,7 @@ mse = mean(abs(err).^2);
 fprintf('MSE = %f\n', mse);
 figure_1 = figure;
 plot(1 : N, abs(d), 1 : N, abs(y), 1 : N, abs(x));
-legend('Desired Signal', 'Estimated Signal', 'Input Signal');
+legend('Desired Signal', 'Estimate', 'Input Signal');
 title('JCLMS');
 
 %% JCGL (Joint Complex Gradient Lattice)
@@ -93,7 +93,8 @@ clear
 % Constants (function input parameters)
 
 SAMPLE_FREQUENCY = 1024;
-SAMPLE_LENGTH = 1024;
+%SAMPLE_LENGTH = 1024;
+SAMLE_LENGTH = 18;
 FILTER_ORDER = 16;
 ALPHA = 0.01;
 
@@ -183,6 +184,16 @@ for n = 1 : 1 : N
 			b_ik(i + 2, k + 2) = conj(a_ik(i + 2, p - k + 2));
 		end
 	end
+	% Cache old values
+	K_i(-1 + 2, :) = K_i(0 + 2, :);
+	K_di(-1 + 2, :) = K_di(0 + 2, :);
+	r_i(-2 + 3, :) = r_i(-1 + 3, :);
+	r_i(-1 + 3, :) = r_i(0 + 3, :);
+	e_i(-1 + 2, :) = e_i(0 + 2, :);
+	e_di(-1 + 2, :) = e_di(0 + 2, :);
+	E_ri(-2 + 3, :) = E_ri(-1 + 3, :);
+	E_ri(-1 + 3, :) = E_ri(0 + 3, :);
+	E_ei(-1 + 2, :) = E_ei(0 + 2, :);
 	err = [err; e_di(0 + 2, p + 2)];
 	diagonal_aik = diag(a_ik);
 	y = [y; -diagonal_aik(0 + 2, p + 2)' * r_i(0 + 3, 0 + 2 : p + 2)'];
@@ -201,10 +212,31 @@ title('JCGL');
 
 clear
 
+% Constants (function input parameters)
+
+SAMPLE_FREQUENCY = 1024;
+SAMPLE_LENGTH = 1024;
+FILTER_ORDER = 16;
+ALPHA = 0.01;
+EPSILON = 0.001;
+
+% Input signal
+
+fs = SAMPLE_FREQUENCY;
+t = (0 : SAMPLE_LENGTH - 1)' / fs; % Time
+s = exp(1j * 2 * pi * 50 * t); % Desired signal
+noise = 0.5 * (randn(size(t)) + 1j * randn(size(t)));
+x = s + noise; % Input signal
+d = s; % Desired signal
+
+p = FILTER_ORDER;
+N = length(x);
+
 % Filter variables
 
 gamma_i = []; % Gain factor
-epsilon_JCLSL = 0.001;
+epsilon_JCLSL = EPSILON;
+alpha_JCLSL = ALPHA;
 
 E_di = 0; % Minimum average squared error
 E_ei = 0; % Minimum forward prediction error
@@ -222,8 +254,39 @@ e_di = []; % Error
 a_ik = []; % Forward prediction (filter) coefficients
 b_ik = []; % Backward prediction (filter) coefficients
 
+% Initialization
+
+K_ei = zeros(p + 1, 1)'; % n
+K_ri = zeros(p + 1, 1)'; % n
+E_ri = epsilon_JCLSL * ones(p + 2, 3)'; % n - 2, n - 1, n
+E_ei = epsilon_JCLSL * ones(p + 2, 2)'; % n - 1, n
+e_i = zeros(p + 2, 2)';
+r_i = zeros(p + 2, 3)';
+K_di = zeros(p + 1, 2)';
+e_di = zeros(p + 2, 2)';
+a_ik = zeros(p + 2);
+b_ik = zeros(p + 2);
+gamma_i = zeros(p + 2, 2)';
+Delta_i = zeros(p + 1, 2)';
+
+for n = 1 : 1 : N
+	% Time update
+	e_i(0 + 2, 0 + 2) = x(n);
+	r_i(0 + 2, 0 + 3) = x(n);
+	E_ri(0 + 2, 0 + 3) = (1 - alpha_JCLSL);
+	E_ei(0 + 2, 0 + 2) = E_ri(0 + 2, 0 + 3);
+	gamma_i(0 + 2, -1 + 2) = 0;
 	
-
-
-
+	% Order update
+	for i = 0 : 1 : p
+		% Lattice
+		if i ~= 0
+			deltaTerm = e_i(0 + 2, -1 + 2) * conj(r_i(-1 + 3, -1 + 2)) / (1 - gamma_i(-1 + 2, -2 + 3));
+			Delta_i(0 + 2, i + 1) = (1 - alpha_JCLSL) * Delta_i(-1 + 2, 0 + 1);
+			Delta_i(0 + 2, i + 1) = Delta_i(0 + 2, i + 1) - deltaTerm;
+			K_ei(0 + 1, 0 + 1) = conj(Delta_i(0 + 2, 0 + 1)) / E_ei(0 + 2, -1 + 2);
+			K_ri(0 + 1, 0 + 1) = Delta_i(0 + 2, 0 + 1) / E_ri(-1 + 2, -1 + 2);
+		end
+	end
+end
 
